891f62759ad95a8d6801b24bcdc2c961f94ac25f
Lachlan Miller <lachlan.miller.1990@outlook.com>
2025-09-06 13:58:49 +1000

    styling

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,6 +1,5 @@
 import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
-import PrimeVue from "primevue/config";
 
-createApp(App).use(PrimeVue).mount("#app");
+createApp(App).mount("#app");
